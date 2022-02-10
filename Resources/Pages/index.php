0f53c1cfd26d064b724572f12464ef7ae8a6a17e
Wendryos <contatowendryos@gmail.com>
2022-02-10 20:13:14 -0300

    Structuring

--- Resources/Pages/index.php
+++ Resources/Pages/index.php
@@ -9,12 +9,26 @@ require_once __DIR__ . "./../../Main/Essential/RedirectNotRoute.php";
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title> Moscovium - Página Inicial </title>
+	 <style type="text/css">
+
+		* { margin: 0; padding:  0; }
+
+		body {
+			background: #30024a;
+		}
+
+
+	</style>
 </head>
  <body>
  	<h1> Hello Word ! </h1>
+ 	<img src="resources/assets/images/icon_cripto.jpg">
+
+
 </body>
  <script type="text/javascript">
 	
+	/*
 	window.addEventListener("load", () => {
 
 		fetch("Main/Controller/UserController.php", {
@@ -27,6 +41,7 @@ require_once __DIR__ . "./../../Main/Essential/RedirectNotRoute.php";
 		});
 
 	});
+	*/
 
 
 </script>
