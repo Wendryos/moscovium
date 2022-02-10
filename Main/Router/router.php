e2a7e128e8b9602bd8ca2d7b263195440557841d
Wendryos <contatowendryos@gmail.com>
2022-02-10 00:29:05 -0300

    Structuring

--- Main/Router/router.php
+++ Main/Router/router.php
@@ -9,18 +9,28 @@ class Router
 	public function __construct() {
 
 		$this->expected_url = [
-			'/',
+			'index',
 			'dashboard'
-
 		];
+
+		return $this->expected_url;
 
 	}
 
 
 	public function RouterRedirect($request) {
 
-		$this->request = $request;
+		 $this->request = $request;
+		 $this->request = str_replace("/moscovium/", "", $this->request);
+		($this->request == "/" ? $this->request = "index" : $this->request = $this->request);
+		($this->request == ""  ? $this->request = "index" : $this->request = $this->request);
 
+		if (in_array($this->request, $this->expected_url)): 
+			include  __DIR__ . "./../../Resources/Pages/{$this->request}.php";
+			
+		endif;
+
+		
 
 	}
 
--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php 
+require_once "./Main/Router/Router.php";
+
+$request = $_SERVER['REQUEST_URI'];
+$router = new Router();
+$router->RouterRedirect($request);
+
 
 
 
