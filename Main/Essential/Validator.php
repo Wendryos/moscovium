faa3d45148c4c224f28f2e0247d9f8f62b36ee67
Wendryos <contatowendryos@gmail.com>
2022-02-09 23:58:32 -0300

    Organizing

--- Main/Essential/Validator.php
+++ Main/Essential/Validator.php
@@ -1,1 +1,19 @@
 <?php 
+
+
+class Validator 
+{
+	protected $request;
+
+	public function Sanitize($request) {
+
+		$this->request = $request;
+		$this->request = filter_var($this->request, FILTER_SANITIZE_STRING);
+		$this->request = trim($this->request);
+		
+		return $this->request;
+
+	}
+
+
+}
--- Main/Router/router.php
+++ Main/Router/router.php
@@ -1,1 +1,28 @@
 <?php 
+
+
+class Router 
+{
+	protected $request;
+	protected $expected_url = array();
+	
+	public function __construct() {
+
+		$this->expected_url = [
+			'/',
+			'dashboard'
+
+		];
+
+	}
+
+
+	public function RouterRedirect($request) {
+
+		$this->request = $request;
+
+
+	}
+
+
+}
